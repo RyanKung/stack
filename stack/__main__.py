c805855dd899058d2430872ce21ce4426b018b64
Ryan.K <ryankung@ieee.org>
2016-04-28 21:21:56 +0800

    updated pargument

--- stack/__main__.py
+++ stack/__main__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
-import os
+# from fabric.main import main
+from stack.args import parser
 
-from fabric.main import main
 if __name__ == '__main__':
+    parser.parse_args()
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(main([os.path.abspath(__file__)]))
+    # sys.exit(main([os.path.abspath(__file__)]))
