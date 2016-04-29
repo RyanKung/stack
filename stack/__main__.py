530a43dfb2cab25f434731b2b387471d538b5260
Ryan.K <ryankung@ieee.org>
2016-04-29 13:16:00 +0800

    import from main

--- stack/__main__.py
+++ stack/__main__.py
@@ -1,41 +1,7 @@
-# -*- coding: utf-8 -*-
 import sys
 import re
-import os
-from typing import Callable
-from stack.args import parser
-from fabric.main import main as fab_main
-from pip import main as pip_main
+from stack.main import main
 
-
-def new(args):
-    from stack.scaffold.template import render
-    template_params = dict(project=args.project, __project__=args.project)
-    return list(render(args.template, template_params))
-
-
-def install(args):
-    return pip_main()
-
-
-def uninstall(args):
-    return pip_main()
-
-
-def fabric_main(args):
-    return fab_main([os.path.abspath(__file__)])
-
-
-def router(argv) -> Callable:
-    return {
-        'new': new,
-        'install': install,
-        'pass': uninstall
-    }.get(argv[1], fabric_main)
-
-
-def main(argv, kwargs):
-    return router(argv)(kwargs)
 
 sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-sys.exit(main(sys.argv, parser.parse_args()))
+sys.exit(main())
