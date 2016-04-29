fc252e6f130863db2eef99552fef27366a8e5adb
Ryan.K <ryankung@ieee.org>
2016-04-29 11:42:50 +0800

    requiremt

---
+++ stack/require/require.py
@@ -0,0 +1,27 @@
+# coding:utf8
+
+# Asynchronous module loader
+# Usage: some_var = require('some_remote_url.tar.gz')
+# Usage: some_var = require('some_remote_url.tar')
+# Usage: some_var = require('some_remote_url.git')
+# Usage: some_var = require('some_remote_url.py')
+'''
+this should implement by wrapping a pip lib
+
+pip -e git repo
+
+
+and do we need to hack the import method
+'''
+
+
+def download():
+    pass
+
+
+def clone_git_repo():
+    pass
+
+
+def extract():
+    pass
