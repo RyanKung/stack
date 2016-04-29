da26362cf3e98f4813297022e998559dda3b6a47
Ryan.K <ryankung@ieee.org>
2016-04-29 11:28:24 +0800

    support fab

--- stack/__main__.py
+++ stack/__main__.py
@@ -38,5 +38,3 @@ def main(argv, kwargs):
     return router(argv[1])(kwargs)
 
 sys.exit(main(sys.argv, parser.parse_args()))
-# sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-# sys.exit(main([os.path.abspath(__file__)]))
