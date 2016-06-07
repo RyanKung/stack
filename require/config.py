06e69911b460e86f7549142a2010f75e228df1ff
Ryan.K <ryankung@ieee.org>
2016-06-07 11:13:12 +0800

    requirepy

--- require/config.py
+++
@@ -1,9 +0,0 @@
-# coding: utf8
-
-import sysconfig
-import distlib.util as util
-
-
-PYTHON_VERSION = sysconfig.get_python_version()
-LIB_PATH = sysconfig.get_path('platlib')
-IS_VENV = util.in_venv()
--- require/require.py
+++
@@ -1,5 +0,0 @@
-# coding:utf8
-
-
-def require_remote(url):
-    pass
